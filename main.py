f53df2fcedba95f64e69b261fab11e2f02bd871c
Sohaib90 <sohaib.arshid101@gmail.com>
2023-05-07 16:47:51 +0200

    Added posts path

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Welcome to the course of API development!"}
+
+@app.get("/posts")
+def get_posts():
+    return {"posts": ["This is post 1", "This is post 2", "This is post 3"]}
