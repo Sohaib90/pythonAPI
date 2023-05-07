9973e3ac1e9eb190d7d743bc05f556ff1fe7232e
Sohaib90 <sohaib.arshid101@gmail.com>
2023-05-07 21:18:15 +0200

    Added post method and pydantic custom schema

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+################################################################
+# Define how data should be in pure, canonical Python; validate it with pydantic.
+from pydantic import BaseModel # create a schema for the post request
+################################################################
+
 
 app = FastAPI()
+
+# pydantic BaseModel extension for custom schema validation
+class Post(BaseModel):
+    title: str
+    content: str
+    pusblish: bool = True
+    # add an optional property
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
@@ -9,3 +24,10 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return {"posts": ["This is post 1", "This is post 2", "This is post 3"]}
+
+# post request: usually sent by the end user to the server to store data
+# return in the function is to indicate whether the request was successful
+@app.post("/createposts")
+def create_post(post : Post):
+    # FastAPI handles the checking of the schema itself when we pass Post(BaseModel) type object
+    return post.dict()
